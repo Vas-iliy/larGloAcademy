10c3a9f8554dc0c57cd9fe80bb9c052bd9cd0f19
Vasiliy <kkolyasev1228@gmail.com>
2020-08-03 15:30:10 +0300

    posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,11 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$factory->define(\App\Post::class, function (Faker $faker) {
+
+    $title = $faker->realText(rand(10, 40));
+    $short_title = mb_strlen($title)>30 ? mb_substr($title, 0, 30) . '...' : $title;
+    $created = $faker->dateTimeBetween('-30 days', '-1 days');
+    $descr = $faker->realText(rand(100, 500));
+
     return [
-        //
+       'title' => $title,
+       'short_title' => $short_title,
+        'descr' => $descr,
+        'author_id' => rand(1, 4),
+        'created_at' => $created,
+        'updated_at' => $created,
     ];
 });
