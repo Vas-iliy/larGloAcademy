30bf14d4cfafd1abf0573751facc37afcbc84bf1
Vasiliy <kkolyasev1228@gmail.com>
2020-08-03 20:50:23 +0300

    delete

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <form action="{{ route('post.update', ['id' => $post->post_id]) }}" method="post" enctype="multipart/form-data">
         @csrf
+        @method('PATCH')
         <h3>Редактировать пост</h3>
         @include('posts.parts.form')
 
