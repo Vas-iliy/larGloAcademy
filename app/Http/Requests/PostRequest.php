6f99167e4a7d645f15d6882a785f3b418ae53095
Vasiliy <kkolyasev1228@gmail.com>
2020-08-04 14:25:04 +0300

    validation

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -13,7 +13,7 @@ class PostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|min:3|max:40',
+            'descr' => 'required|min:10|max:550',
+            'img' => 'mimes:jpeg,jpg,png|max:5000'
         ];
     }
 }
